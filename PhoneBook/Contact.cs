fc7e4aa87351d79f24a73fb7aba12f3e198fdfe7
pawel-zajac <z.pawelbb@gmail.com>
2019-01-12 15:05:28 +0100

    Add classes responsible for operations and storing contacts

---
+++ PhoneBook/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    class Contact
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+        public Contact(string name, string number)
+        {
+            this.Name = name;
+            this.Number = number;
+        }
+    }
+
+    
+
+    
+}
